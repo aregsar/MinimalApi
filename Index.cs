6016a32771ebbebe5b2d21129d82e095708ad380
Areg Sarkissian <aregsar@gmail.com>
2021-08-25 14:04:28 -0700

    update

--- Index.cs
+++ Index.cs
@@ -4,7 +4,14 @@ namespace Example
     public partial class Sample
     {
         // Func<string>? x = null;
-        public static Func<string> Index = () => "Hello World!";
+        //public static Func<string> Index3 = () => "Hello World!";
+
+        public static string Index() => "Hello World!";
+
+        // public static string Index2()
+        // {
+        //     return "Hello World!";
+        // }
     }
 
 
